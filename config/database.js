4b617d1edf65b8fe25da5a59a6db45d482e2bd4c
sazzad-anwar <sazzadzihan@gmail.com>
2021-12-16 16:45:07 +0600

    db config error fixed

--- config/database.js
+++ config/database.js
@@ -10,7 +10,9 @@ module.exports = ({ env }) => ({
       database: config.database,
       user: config.user,
       password: config.password,
-      ssl: false
+      ssl: {
+        rejectUnauthorized: false
+      }
     },
     debug: false,
   },
