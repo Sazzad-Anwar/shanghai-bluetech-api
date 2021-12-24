6d1bddbaf3b966595f24f4385e2aaa1cbf50a2b0
sazzad-anwar <sazzadzihan@gmail.com>
2021-12-24 21:45:16 +0600

    database connection string edited

--- config/database.js
+++ config/database.js
@@ -10,9 +10,9 @@ module.exports = ({ env }) => ({
       database: config.database,
       user: config.user,
       password: config.password,
-      // ssl: {
-      //   rejectUnauthorized: false
-      // }
+      ssl: {
+        rejectUnauthorized: false
+      }
     },
     debug: false,
   },
